7a0783a3c67d4ff1d3d5090ba7b8f32e362c15f5
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-18 02:43:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,12 @@
 # Write your code here.
 def badge_maker(name)
-  "Hello, my name is #{name}."  
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_list)
+  all_badges = []
+  name_list.each do |name|
+    all_badges.push(badge_maker(name))
+  end
+  all_badges
 end
